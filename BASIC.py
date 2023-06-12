c2a521808cf678dbbe67dc6a584e0421f1517805
Piyush Negi <negipiyush125@gmail.com>
2023-06-12 23:47:04 +0530

    hello world

--- BASIC.py
+++ BASIC.py
@@ -1,1 +1,14 @@
 print("hello World")
+# VARIABLES
+a = 4
+print(a)  # integer datatype
+a=4.56 # floaT data type
+print(a)
+c =True  # or False boolean
+print(c)
+
+d = "Piyush"  #string data type
+print(d)
+
+e = None
+print(e)
