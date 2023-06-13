761b5c79e5ea19ff8da97a18ad859d85eb222516
Piyush Negi <negipiyush125@gmail.com>
2023-06-13 18:34:57 +0530

    up

--- BASIC.py
+++ BASIC.py
@@ -2,13 +2,22 @@ print("hello World")
 # VARIABLES
 a = 4
 print(a)  # integer datatype
-a=4.56 # floaT data type
+a = 4.56  # float data type
 print(a)
-c =True  # or False boolean
+c = True  # or False boolean
 print(c)
 
-d = "Piyush"  #string data type
+d = "Piyush"  # string data type
 print(d)
 
 e = None
 print(e)
+
+''' TYPECASTING IN PYTHON (CONVERTING ONE DATATYPE TO OTHER )'''
+z = "6"  # Here the value of z is in string form
+'''print("the value of z is", z+1)'''  # THis will give erro becoz z which is a string is being added to a integer
+print("the value of z is", int(z)+1)  # THis will give output coz first z is coverted into the same data type
+print("the value of z is", z + "1")  # THis will give output coz both are of the same data type
+e = 6
+print("the value of e is", e + 1.34)  # will work as well as vice versa
+
