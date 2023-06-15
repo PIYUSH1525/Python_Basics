244bc5fc19c1e24832e340395212dd2b7865d259
Piyush Negi <negipiyush125@gmail.com>
2023-06-15 10:34:34 +0530

    OP UP

--- BASIC.py
+++ BASIC.py
@@ -15,9 +15,16 @@ print(e)
 
 ''' TYPECASTING IN PYTHON (CONVERTING ONE DATATYPE TO OTHER )'''
 z = "6"  # Here the value of z is in string form
-'''print("the value of z is", z+1)'''  # THis will give erro becoz z which is a string is being added to a integer
-print("the value of z is", int(z)+1)  # THis will give output coz first z is coverted into the same data type
+'''print("the value of z is", z+1)'''  # THis will give
+#
+#
+#
+
+
+# error becoz z which is a string is being added to a integer
+print("the value of z is", int(z)+1)  # THis will give output coz first z is converted into the same data type
 print("the value of z is", z + "1")  # THis will give output coz both are of the same data type
 e = 6
+print("the value of z is", z + "1")  # THis will give output coz both are of the same data type
 print("the value of e is", e + 1.34)  # will work as well as vice versa
-random test gebnerter
+
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 # for replicating line multiple time use CTRL+D
 # for commenting multiple line select then and press CTRL+/
 # for commenting multiple line use ''' comment here  '''  ===> this is called multiline comments.
+# hold ALT key for multi cursor 
