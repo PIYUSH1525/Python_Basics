f79fb3e1c34b2980376c0f7e9dc2967dc45d7676
Piyush Negi <negipiyush125@gmail.com>
2023-06-15 10:33:51 +0530

    OP UP

---
+++ operators.py
@@ -0,0 +1,14 @@
+number1 = 20
+number2 = 8
+
+# ARITHMETIC OPERATIONS
+print("number1 + number2 is", number1 + number2)
+print("number1 - number2 is", number1 - number2)
+print("number1 / number2 is", number1 / number2)
+print("number1 * number2 is", number1 * number2)
+                 # ADVANCE
+print("number1 // number2 is", number1 // number2)  # USED FOR SHOWING ONLY VALUES BEFORE THE POINT NOT AFTER IT
+print("number1 ** number2 is", number1 ** number2)  # USED  FOR EXPONENTIAL POWER OF THE NUMBER
+print("number1 % number2 is", number1 % number2)  # USED FOR GIVING REMAINDER AFTER DIVISION
+
+                   # ASSIGNMENT OPERATORS
