df7ddc6ceac65a21f3a93fb95ebebc5d7657ca09
Piyush Negi <negipiyush125@gmail.com>
2023-06-16 13:33:34 +0530

    opp up

--- operators.py
+++ operators.py
@@ -6,10 +6,37 @@ print("number1 + number2 is", number1 + number2)
 print("number1 - number2 is", number1 - number2)
 print("number1 / number2 is", number1 / number2)
 print("number1 * number2 is", number1 * number2)
-                 # ADVANCE
+# ADVANCE
 print("number1 // number2 is", number1 // number2)  # USED FOR SHOWING ONLY VALUES BEFORE THE POINT NOT AFTER IT
 print("number1 ** number2 is", number1 ** number2)  # USED  FOR EXPONENTIAL POWER OF THE NUMBER
 print("number1 % number2 is", number1 % number2)  # USED FOR GIVING REMAINDER AFTER DIVISION
 
-                   # ASSIGNMENT OPERATORS
-THe operastoers are
+# ASSIGNMENT OPERATORS
+''' These operators are used for assigning values to variables  '''
+z = 4
+z += 3      # HEre output is calculated line by line
+z -= 4
+# z /= 3
+print(z)
+
+# COMPARISION OPERATOR
+x = 10
+y = 10
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x>=y)
+print(x==y) # == IS A COMPARSION OPEATOR WHILE = IS A ASSIGNMENT OPERATOR
+
+# LOGICAL OPRAATORS
+x = 8
+y = 3
+z = 8
+print(x!=z and x>y)  # BOTH THE CONDITION SHOULD BE COMPULSARY TRUE IN AND OPRATOR
+print(x==z and x>y)
+# OR OPERATOR
+print(x==z or x>y)   # THE OUTPUT WILL BE TRUE WHEN EITHER OF THEM OR BOTH OF THE CONDITIONS  ARE TRUE
+print(x==z or x>y)
+
+
+
