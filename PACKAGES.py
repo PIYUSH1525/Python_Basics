be130535a904bf5f40e90bb8b7f0b7f8af27793b
Piyush Negi <negipiyush125@gmail.com>
2023-06-22 11:29:23 +0530

    String

--- PACKAGES.py
+++ PACKAGES.py
@@ -1,4 +1,5 @@
-import pandas  # inporting  the package
-
-d = pandas.read_excel("file.xlsx")   # READING THE DATA INTO A VARIABLE
+import pandas as pd  # importing  the package
+# as is used to rename the package to reduce the length name or for easy programming
+d = pd.read_excel("file.xlsx")   # READING THE DATA INTO A VARIABLE
 print(d)   # PRINTING THE DATA
+
---
+++ string.py
@@ -0,0 +1,10 @@
+string_name = "piyush"        # BOTH THE "" AND '' WILL WORK
+string_name2 = 'Negi'         # BOTH THE "" AND '' WILL WORK
+print(string_name)
+print(string_name2)
+# SLICING THE STRINGS
+print(string_name[0:4])  # n to n-1 INDEX
+print(string_name[3:4])  # [STARTING INDEX NUMBER : ENDING INDEX NUMBER + 1]
+print(string_name.capitalize()) # USED TO CAPTILISSE THE FIRST LETTER OF THE STRING
+print(string_name.count("i"))  # USED TO COUNT THE OCCURANCE
+print(string_name.isalnum())
