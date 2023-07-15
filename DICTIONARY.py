bcc1cd64f7a53936b273c6204c2660fe41e61c69
Piyush Negi <negipiyush125@gmail.com>
2023-07-15 10:51:13 +0530

    CHANGES

---
+++ DICTIONARY.py
@@ -0,0 +1,5 @@
+# dictionary is basically a combination of a key and a value { key: value}
+piyush_dict = {"piyush":17342, "amit" :16951}
+print(piyush_dict["amit"])
+print(piyush_dict["piyush"])
+print(type(piyush_dict["amit"]))
--- SETS AND METHODS.py
+++ SETS AND METHODS.py
@@ -1,6 +1,8 @@
 a = { 3,5,4,6,6}
 b = {2,5,5,5,5,5,5,7,8,9}                # one element is counted only one time
 c = {3,5,4,6}                            # a nd c are both same sets
+d = set()                                # EMPTY SET
+print(type(d))
 print(b)
 a.clear()
 print(b.pop())                           # random element will pop
